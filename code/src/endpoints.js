d2d5135fc337d928e502d62e72a8ca1a288365e6
BB1986-co <143395483+BB1986-co@users.noreply.github.com>
2023-12-22 10:43:08 +0000

    chore updadete befor lunch

--- code/src/endpoints.js
+++ code/src/endpoints.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
+
+//Router 
 
 
 app.use(express.json());
@@ -46,6 +49,47 @@ app.patch('/tasks/:id', (request, response) => {
 app.delete('/tasks/:id',(request, respones) => {
     tasks = tasks.filter((task) => task.id !== request.params.id);
     respones.status(200).send(tasks);
+})
+
+// Login
+
+app.post('/login', function (request, response) {
+	const { email, password } = request.body
+
+	// Check the credentials against store
+	if (email?.toLowerCase() == secretAdminCredentials.email && password == secretAdminCredentials.password) {
+
+		// Link email to session
+		request.session.email = email
+
+		return response.status(200).json({ email: request.session.email })
+	}
+
+  return response.status(401).json({ error: "Invalid credentials" })
+});
+
+app.get('/verify', function (request, response) {
+	
+	// Check if email is set in session
+	if (request.session.email) {
+		return response.status(200).json({ email: request.session.email })
+	}
+
+  return response.status(401).json({ error: "Not logged in" })
+})
+
+app.delete('/logout', function (request, response) {
+
+	// Check if email is set in session
+	if (request.session.email) {
+
+		// Reset link of session to email
+		request.session.email = null
+
+		return response.status(204).send()
+	}
+
+  return response.status(401).json({ error: "Not logged in" })
 })
 
 app.listen(port, ()=>{
