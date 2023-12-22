722c3c4146a794797cd4368947b2bc325925f8d0
BB1986-co <143395483+BB1986-co@users.noreply.github.com>
2023-12-22 12:24:19 +0000

    chore insert error handling

--- README.md
+++ README.md
@@ -24,4 +24,3 @@ Dokumentation:
 
 
 
-
--- code/src/endpoints.js
+++ code/src/endpoints.js
@@ -10,6 +10,7 @@ const verify = require('./verify');
 app.use('/verify',verify);
 
 const logout = require('./logout');
+const { json } = require('stream/consumers');
 app.use('/logout', logout);
 
 // Json middleware Zugriff
@@ -22,52 +23,57 @@ let tasks = [
 ];
 
 // Endpunkte
-app.get('/tasks', (request, response) => {
-    response.status(200).send(tasks);
-});
-
-app.post('/tasks', (request, response) => {
-    const newTask = request.body;
-    const newID = request.body;
-    while(newID !== tasks.params.id){
-        newID['id'] = new Number();
-    }
-    newTask['erstellungsdatum'] = new Date().toISOString();
-    tasks = [...tasks, request.body];
-
-    tasks.push(request.body);
-    response.status(201).send(tasks);
-})
-
-app.get('/tasks/:id', (request, response) => {
-    if(id !== tasks.params.id){
-        response.status(404).send("id nicht vorhanden!");
-    }else{
-        response.status(200).send(tasks.filter((task) => task.id === request.params.id));
-    }
+try {
+    app.get('/tasks', (request, response) => {
+        response.status(200).send(tasks);
+    });
     
-});
-
-app.patch('/tasks/:id', (request, response) => {
-    const keys = Object.keys(request.body);
-    const oldTask = tasks.find((task) => task.id === task.params.id);
-    if(oldTask !== tasks.params.id){
-        response.status(404).send("id nicht vorhanden!");
-    }else{
-        keys.forEach((key) => oldTask[key] = request.body [key]);
-        tasks = tasks.map((task) => task.id === request.params.id ? oldTask : task);
+    app.post('/tasks', (request, response) => {
+        const newTask = request.body;
+        const newID = request.body;
+        while(newID !== tasks.params.id){
+            newID['id'] = new Number();
+        }
+        newTask['erstellungsdatum'] = new Date().toISOString();
+        tasks = [...tasks, request.body];
+    
+        tasks.push(request.body);
         response.status(201).send(tasks);
-    } 
-});
-
-app.delete('/tasks/:id',(request, respones) => {
-    if(id !== tasks.params.id){
-        response.status(404).send("id nicht vorhanden!");
-    }else{
-        tasks = tasks.filter((task) => task.id !== request.params.id);
-        respones.status(200).send(tasks);
-    } 
-})
+    })
+    
+    app.get('/tasks/:id', (request, response) => {
+        if(id !== tasks.params.id){
+            response.status(404).send("id nicht vorhanden!");
+        }else{
+            response.status(200).send(tasks.filter((task) => task.id === request.params.id));
+        }
+        
+    });
+    
+    app.patch('/tasks/:id', (request, response) => {
+        const keys = Object.keys(request.body);
+        const oldTask = tasks.find((task) => task.id === task.params.id);
+        if(oldTask !== tasks.params.id){
+            response.status(404).send("id nicht vorhanden!");
+        }else{
+            keys.forEach((key) => oldTask[key] = request.body [key]);
+            tasks = tasks.map((task) => task.id === request.params.id ? oldTask : task);
+            response.status(201).send(tasks);
+        } 
+    });
+    
+    app.delete('/tasks/:id',(request, respones) => {
+        if(id !== tasks.params.id){
+            response.status(404).send("id nicht vorhanden!");
+        }else{
+            tasks = tasks.filter((task) => task.id !== request.params.id);
+            respones.status(200).send(tasks);
+        } 
+    })
+    
+} catch (response) {
+    response.status(500);
+}
 
 app.listen(port, ()=>{
     console.log(`server runs on port: 3000 ${port}`);
